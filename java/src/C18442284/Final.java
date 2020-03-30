ab6d3cdc81cf9eeee073fbb62b16686e2c0e1515
StoopDJ <amirakak@gmail.com>
2020-03-30 17:57:38 +0100

    CUBE CLASS

--- java/src/C18442284/Final.java
+++ java/src/C18442284/Final.java
@@ -13,24 +13,32 @@ public class Final extends Visual
     AudioPlayer song;
     FFT fft;
 
+    // Variables that define the "areas" of the spectrum
     float specLow = (float) 0.03;
     float specMid = (float) 0.125;
     float specHi = (float) 0.20;
 
+    // There is therefore 64% of the possible spectrum which will not be used.
+    // These values ​​are generally too high for the human ear anyway.
+
+    // Score values ​​for each zone
     float scoreLow = 0;
     float scoreMid = 0;
     float scoreHi = 0;
 
+    // Previous values, to soften the reduction
     float oldScoreLow = scoreLow;
     float oldScoreMid = scoreMid;
     float oldScoreHi = scoreHi;
 
-
+    // Softening value
     float scoreDecreaseRate =25;
 
+    // Cubes that appear in space
     int nbCube;
     Cube[] cubes;
 
+    // Lines that appear on the sides
     int nbMurs = 500;
     Mur[] murs;
     
@@ -65,11 +73,25 @@ public class Final extends Visual
         colorMode(HSB);
         noCursor();
         
-        setFrameSize(256);
+        setFrameSize(256);  
+    }
 
-       
-       
+    public void draw()
+    {
 
+    }
+
+    // Class for cubes floating in space
+    class Cube
+    {
+        // Z position of "spawn" and maximum Z position
+        float startingZ =  - 10000 ;
+        float maxZ =  1000 ;
         
+        // Position values
+        float x, y, z;
+        float rotX, rotY, rotZ;
+        float sumRotX, sumRotY, sumRotZ;
+
     }
 }

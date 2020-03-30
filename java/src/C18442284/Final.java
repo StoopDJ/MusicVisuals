e5c2c679f462ba884861abdc7244662401890d1f
StoopDJ <amirakak@gmail.com>
2020-03-30 17:38:21 +0100

    import

--- java/src/C18442284/Final.java
+++ java/src/C18442284/Final.java
@@ -1,7 +1,10 @@
 package C18442284;
 
 import ddf.minim.AudioPlayer;
+import ddf.minim.analysis.*;
+import ddf.minim.*;
 import ie.tudublin.Visual;
+
 
 public class Final extends Visual 
 {
@@ -11,5 +14,25 @@ public class Final extends Visual
         fullScreen(P3D, SPAN);
     }
 
-    
+    public void keyPressed() {
+        if (key == ' ') {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+
+        }
+
+    }
+    public void setup()
+    {
+        colorMode(HSB);
+        noCursor();
+        
+        setFrameSize(256);
+
+        startMinim();
+        loadAudio("Visuals.mp3");
+        //getAp().play();
+        //startListening(); 
+        
+    }
 }

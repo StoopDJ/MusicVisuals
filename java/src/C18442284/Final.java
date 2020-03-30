5841deb392e5230a53006f7465578338ebbcabbb
StoopDJ <amirakak@gmail.com>
2020-03-30 17:48:37 +0100

    start code

--- java/src/C18442284/Final.java
+++ java/src/C18442284/Final.java
@@ -9,6 +9,32 @@ import ie.tudublin.Visual;
 public class Final extends Visual 
 {
 
+    Minim minim;
+    AudioPlayer song;
+    FFT fft;
+
+    float specLow = (float) 0.03;
+    float specMid = (float) 0.125;
+    float specHi = (float) 0.20;
+
+    float scoreLow = 0;
+    float scoreMid = 0;
+    float scoreHi = 0;
+
+    float oldScoreLow = scoreLow;
+    float oldScoreMid = scoreMid;
+    float oldScoreHi = scoreHi;
+
+
+    float scoreDecreaseRate =25;
+
+    int nbCube;
+    Cube[] cubes;
+
+    int nbMurs = 500;
+    Mur[] murs;
+    
+
     public void settings() {
         // size(800, 800, P3D);
         fullScreen(P3D, SPAN);
@@ -23,16 +49,19 @@ public class Final extends Visual
 
     }
     public void setup()
-    {
+    {   
+        minim = new Minim(this);
+
+        song = minim.loadFile("Visuals.mp3");
+
         colorMode(HSB);
         noCursor();
         
         setFrameSize(256);
 
-        startMinim();
-        loadAudio("Visuals.mp3");
-        //getAp().play();
-        //startListening(); 
+       
+       
+
         
     }
 }
